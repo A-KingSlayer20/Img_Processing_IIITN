5ad38c5a7bb9cf5cc5859e49f5e34d8f8050829e
Abhi <abhimanyux99@gmail.com>
2024-08-09 11:30:59 +0530

    Theory project successful

--- Theory_1.py
+++ Theory_1.py
@@ -1,1 +1,19 @@
+# Image conversion to grayscale and B+W
 
+
+import cv2 as cv
+
+img=cv.imread("D:\ThirdYear_Books\Img_Processing_IIITN\Photos\park.jpg")
+cv.imshow("Img",img)
+
+grey_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+# def rescaleFrame(frame,scale=0.75) :
+#     width=int(frame.shape[1]*scale)
+#     height=int(frame.shape[0]*scale)
+
+#     dimensions=(width,height)
+
+#     return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+cv.waitKey(0)
