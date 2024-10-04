de5de47e461e126c223f9287a8e1b4395e1c2152
Abhi <abhimanyux99@gmail.com>
2024-10-04 09:49:03 +0530

    Code Update

--- Theory_10.py
+++ Theory_10.py
@@ -1,1 +1,28 @@
 # Code for implementation of k-means clustering algorithm
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+# Generate random data with 3 centers (clusters)
+X, y = make_blobs(n_samples=300, centers=3, cluster_std=0.60, random_state=0)
+
+# Create a KMeans model with 3 clusters
+kmeans = KMeans(n_clusters=3)
+
+# Fit the model to the data
+kmeans.fit(X)
+
+# Get the cluster centroids and labels for each point
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+# Plotting the results
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
+
+# Plot the centroids
+plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=200, marker='x')
+plt.title("K-Means Clustering")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.show()
--- Theory_11.py
+++ Theory_11.py
@@ -1,1 +1,38 @@
 # Code for Discrete Wavelength Transform
+import pywt
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate a sample signal
+t = np.linspace(0, 1, num=1024)
+signal = np.sin(2 * np.pi * 5 * t) + 0.5 * np.sin(2 * np.pi * 50 * t)
+
+# Perform Discrete Wavelet Transform (DWT) using the 'db4' wavelet
+wavelet = 'db4'
+coeffs = pywt.wavedec(signal, wavelet)
+
+# Coefficients consist of (cA_n, cD_n, cD_n-1, ..., cD_1)
+# cA_n is the approximation coefficients
+# cD_n, ..., cD_1 are the detail coefficients at different levels
+cA, *cD = coeffs
+
+# Plot the original signal
+plt.figure(figsize=(10, 6))
+plt.subplot(2, 1, 1)
+plt.plot(t, signal)
+plt.title('Original Signal')
+
+# Plot the approximation coefficients (cA) and detail coefficients (cD)
+plt.subplot(2, 1, 2)
+plt.plot(cA)
+plt.title('Approximation Coefficients (cA)')
+plt.tight_layout()
+
+plt.show()
+
+# You can also access the detail coefficients and plot them as needed
+for i, detail_coeff in enumerate(cD, start=1):
+    plt.figure(figsize=(10, 3))
+    plt.plot(detail_coeff)
+    plt.title(f'Detail Coefficients (cD_{i})')
+    plt.show()
